1bb6c975c924095bd888fd7b96433675be964e76
SagarSingh9950 <ssagar77040@gmail.com>
2026-02-22 20:53:46 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const MarvelServer = http.createServer((req , res) => {
- console.log("req.headers");
- res.end("RAM RAM FROM MARVEL SERVER");
-});
-
-MarvelServer.listen(3000, () => console.log("server Started!"));
- 
